131d5c7d38261cf91b8cd44a237d56dfe757e288
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-08-06 14:19:49 +0700

    implement set rows per page

--- resources/js/Components/Table/DropdownSetRows.jsx
+++ resources/js/Components/Table/DropdownSetRows.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import getQueryParams from "@/Utilities/getQueryParams";
+import { router } from "@inertiajs/react";
 
 export default function DropdownSetRows() {
+    const currentQueryParams = getQueryParams();
     const optionLimits = [15, 25, 50, 100, 150];
+    const [currentLimit, setCurrentLimit] = useState(0);
+
+    useEffect(() => {
+        setCurrentLimit(currentQueryParams.limit);
+    }, [currentQueryParams]);
+
+    const handleSelectPerPageRows = (e) => {
+        const routeParams = { ...currentQueryParams };
+
+        routeParams.limit = e.target.value;
+        router.visit(route("articles.index", routeParams), {
+            method: "get",
+        });
+
+        setCurrentLimit(e.target.value);
+    };
+
     return (
         <div className="join items-center">
             <label className="label w-full">
                 <span className="label-text">Rows:</span>
             </label>
-            <select className="select w-full max-w-xs">
+            <select
+                className="select w-full max-w-xs"
+                value={currentLimit}
+                onChange={handleSelectPerPageRows}
+            >
+                <option key={currentLimit} disabled>
+                    {currentLimit}
+                </option>
                 {optionLimits.map((limit) => {
-                    return <option key={limit}>{limit}</option>;
+                    if (currentLimit !== limit) {
+                        return <option key={limit}>{limit}</option>;
+                    }
                 })}
             </select>
         </div>
