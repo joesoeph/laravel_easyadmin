2524dc82e72de9e2e39a4dc696f23f9ae734408c
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-08-16 11:55:57 +0700

    convert login to using ant design

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,97 +1,87 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect } from "react";
+import GuestLayout from "@/Layouts/GuestLayout";
+import { Head, useForm } from "@inertiajs/react";
+import { Button, Checkbox, Form, Input } from "antd";
+import ButtonLink from "@/Components/Table/ButtonLink";
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: false,
     });
 
     useEffect(() => {
         return () => {
-            reset('password');
+            reset("password");
         };
     }, []);
 
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route('login'));
+    // submit form
+    const onFinish = (values) => {
+        post(route("login"));
     };
 
     return (
         <GuestLayout>
             <Head title="Log in" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+            {status && (
+                <div className="tw-mb-4 tw-font-medium tw-text-sm tw-text-green-600">
+                    {status}
+                </div>
+            )}
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
+            <Form layout="vertical" onFinish={onFinish} autoComplete="off">
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    required
+                    validateStatus={errors.email ? "error" : ""}
+                    help={errors.email ? errors.email : ""}
+                >
+                    <Input
                         value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
+                        onChange={(e) => setData("email", e.target.value)}
                     />
+                </Form.Item>
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    required
+                    validateStatus={errors.password ? "error" : ""}
+                    help={errors.password ? errors.password : ""}
+                >
+                    <Input.Password
                         value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        onChange={(e) => setData("password", e.target.value)}
                     />
+                </Form.Item>
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ml-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ml-4" disabled={processing}>
+                <Form.Item name="remember">
+                    <Checkbox
+                        checked={data.remember}
+                        onChange={(e) => setData("remember", e.target.checked)}
+                    >
+                        Remember me
+                    </Checkbox>
+                </Form.Item>
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        loading={processing}
+                    >
                         Log in
-                    </PrimaryButton>
-                </div>
-            </form>
+                    </Button>
+                    {canResetPassword && (
+                        <ButtonLink href={route("password.request")}>
+                            Forgot yout password?
+                        </ButtonLink>
+                    )}
+                </Form.Item>
+            </Form>
         </GuestLayout>
     );
 }
