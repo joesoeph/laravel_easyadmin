6531e058f58b98de4ded4c0fc9a502d8d86bb4e5
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-08-16 12:26:48 +0700

    convert forgot password UI to using ant design

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -1,50 +1,59 @@
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, useForm } from '@inertiajs/react';
+import GuestLayout from "@/Layouts/GuestLayout";
+import { Head, useForm } from "@inertiajs/react";
+import { Button, Form, Input, Typography, Alert } from "antd";
+
+const { Text } = Typography;
 
 export default function ForgotPassword({ status }) {
     const { data, setData, post, processing, errors } = useForm({
-        email: '',
+        email: "",
     });
 
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route('password.email'));
+    const onFinish = (values) => {
+        post(route("password.email"));
     };
 
     return (
         <GuestLayout>
             <Head title="Forgot Password" />
 
-            <div className="mb-4 text-sm text-gray-600">
-                Forgot your password? No problem. Just let us know your email address and we will email you a password
-                reset link that will allow you to choose a new one.
-            </div>
+            <Text type="secondary">
+                Forgot your password? No problem. Just let us know your email
+                address and we will email you a password reset link that will
+                allow you to choose a new one.
+            </Text>
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
-
-            <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData('email', e.target.value)}
-                />
-
-                <InputError message={errors.email} className="mt-2" />
-
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Email Password Reset Link
-                    </PrimaryButton>
+            {status && (
+                <div className="tw-my-6">
+                    <Alert message={status} type="success" />
                 </div>
-            </form>
+            )}
+
+            <Form layout="vertical" onFinish={onFinish} autoComplete="off">
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    required
+                    validateStatus={errors.email ? "error" : ""}
+                    help={errors.email ? errors.email : ""}
+                >
+                    <Input
+                        value={data.email}
+                        onChange={(e) => setData("email", e.target.value)}
+                    />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        loading={processing}
+                        className="tw-mt-4"
+                    >
+                        Email Password Reset Link
+                    </Button>
+                </Form.Item>
+            </Form>
         </GuestLayout>
     );
 }
