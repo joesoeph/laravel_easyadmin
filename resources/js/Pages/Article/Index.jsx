a63f0c1bf46ce6b503c506abb5608d974abae41f
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-08-17 20:33:06 +0700

    implement server side ant design table

--- resources/js/Pages/Article/Index.jsx
+++ resources/js/Pages/Article/Index.jsx
@@ -1,85 +1,170 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head, Link } from "@inertiajs/react";
-import SimpleTable from "@/Components/Table/SimpleTable";
-import DropdownActionRow from "@/Components/Table/DropdownActionRow";
+import { Head, router } from "@inertiajs/react";
+import ButtonLink from "@/Components/Table/ButtonLink";
+import { Button, Dropdown, Space, Table } from "antd";
+import { useEffect, useState } from "react";
+import QueryString from "qs";
+import { MoreOutlined, DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
-export default function Index({ auth, articles }) {
+const items = [
+    {
+        label: (
+            <Space>
+                <EditOutlined />
+                Edit
+            </Space>
+        ),
+        key: "edit",
+    },
+    {
+        label: (
+            <Space>
+                <DeleteOutlined />
+                Delete
+            </Space>
+        ),
+        key: "delete",
+        danger: true,
+    },
+];
+
+const columns = [
+    {
+        title: "Title",
+        dataIndex: "title",
+        sorter: true,
+    },
+    {
+        title: "Content",
+        dataIndex: "content",
+        sorter: true,
+    },
+    {
+        title: "Action",
+        dataIndex: "data",
+        render: (text, record, index) => {
+            return (
+                <Dropdown
+                    menu={{
+                        items,
+                        onClick: ({ key }) => {
+                            switch (key) {
+                                case "edit":
+                                    console.log(key, record.id);
+                                    break;
+
+                                default:
+                                    console.log(key, record.id);
+                                    break;
+                            }
+                        },
+                    }}
+                    trigger={["click"]}
+                >
+                    <Button size="large" shape="circle">
+                        <MoreOutlined />
+                    </Button>
+                </Dropdown>
+            );
+        },
+    },
+];
+
+const getTableParams = (params) => {
+    const tableParams = {
+        page: params.pagination?.current,
+        limit: params.pagination?.pageSize,
+    };
+
+    if (params.order !== undefined && params.field !== undefined) {
+        tableParams.sort = `${params?.order === "descend" ? "-" : ""}${
+            params?.field
+        }`;
+    }
+
+    return tableParams;
+};
+
+export default function Index({ auth }) {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(false);
+    const [tableParams, setTableParams] = useState({
+        pagination: {
+            current: 1,
+            pageSize: 10,
+        },
+    });
+
+    const fetchData = () => {
+        setLoading(true);
+        fetch(
+            `http://127.0.0.1:8000/articles/list?${QueryString.stringify(
+                getTableParams(tableParams)
+            )}`
+        )
+            .then((response) => response.json())
+            .then((payload) => {
+                setData(payload.data);
+                setLoading(false);
+                setTableParams({
+                    ...tableParams,
+                    pagination: {
+                        ...tableParams.pagination,
+                        total: payload.total,
+                        pageSize: payload.per_page,
+                        current: payload.current_page,
+                    },
+                });
+            });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [JSON.stringify(tableParams)]);
+
+    const handleTableChange = (pagination, filters, sorter) => {
+        setTableParams({
+            pagination,
+            filters,
+            ...sorter,
+        });
+
+        // `dataSource` is useless since `pageSize` changed
+        if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+            setData([]);
+        }
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                <h2 className="tw-font-semibold tw-text-xl tw-text-gray-800 tw-leading-tight">
                     Articles
                 </h2>
             }
         >
             <Head title="Articles" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                        <Link
-                            href={route("articles.create")}
-                            className="btn btn-neutral"
-                        >
-                            + Create new
-                        </Link>
-                        <SimpleTable
-                            className="mt-6 text-gray-900"
-                            paginationData={articles}
-                            columnSearch="title"
-                            dataMapping={[
-                                {
-                                    columnName: "No.",
-                                    resource: function (
-                                        item,
-                                        rowIndex,
-                                        colIndex
-                                    ) {
-                                        return ++rowIndex;
-                                    },
-                                },
-                                {
-                                    columnName: "Title",
-                                    resource: function (
-                                        item,
-                                        rowIndex,
-                                        colIndex
-                                    ) {
-                                        return item.title;
-                                    },
-                                },
-                                {
-                                    columnName: "Content",
-                                    resource: function (
-                                        item,
-                                        rowIndex,
-                                        colIndex
-                                    ) {
-                                        return item.content;
-                                    },
-                                },
-                                {
-                                    columnName: "Username",
-                                    resource: function (
-                                        item,
-                                        rowIndex,
-                                        colIndex
-                                    ) {
-                                        return item.user.name;
-                                    },
-                                },
-                                {
-                                    columnName: "Actions",
-                                    resource: function (
-                                        item,
-                                        rowIndex,
-                                        colIndex
-                                    ) {
-                                        return <DropdownActionRow />;
-                                    },
-                                },
-                            ]}
+            <div className="tw-py-12">
+                <div className="tw-max-w-7xl tw-mx-auto sm:tw-px-6 lg:tw-px-8">
+                    <div className="tw-bg-white tw-overflow-hidden tw-shadow-sm sm:tw-rounded-lg tw-p-6">
+                        <div className="tw-text-right">
+                            <ButtonLink
+                                href={route("articles.create")}
+                                type="primary"
+                            >
+                                + Create new
+                            </ButtonLink>
+                        </div>
+                        <Table
+                            columns={columns}
+                            dataSource={data}
+                            loading={loading}
+                            rowKey={(record) => record.id}
+                            pagination={tableParams.pagination}
+                            onChange={handleTableChange}
+                            className="tw-mt-6"
                         />
                     </div>
                 </div>
